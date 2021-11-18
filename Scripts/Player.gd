37792ac373faab3dbb6f99f87fe3e04db77595eb
heygwangjin <cer3@ajou.ac.kr>
2021-11-18 21:23:06 +0900

    feat: Add Movement of player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,21 +1,26 @@
 extends KinematicBody2D
 
-var speed = 300
-var gravity = 300
-var jump = -2500
-var velocity = Vector2()
+const ACCELARATION = 300
+const GRAVITY = 20
+const JUMP_SPEED = 600
+var motion = Vector2()
 
 func _physics_process(delta):
-	if Input.is_action_pressed("right"):
+	# Movements
+	motion.y += GRAVITY
+	if Input.is_action_pressed("ui_right"):
+		motion.x = ACCELARATION
 		$Sprite.play("walk")
-		velocity.x = speed
-	if Input.is_action_pressed("left"):
-		velocity.x = -speed
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -ACCELARATION
+		$Sprite.play("walk")
+	else:
+		$Sprite.play("idle")
 		
 	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = jump
+		motion.y -= JUMP_SPEED
 		
-	velocity.y = velocity.y + gravity	
-	velocity.x = lerp(velocity.x,0,0.4)
+	motion.x = lerp(motion.x,0,0.4)
+	motion = move_and_slide(motion, Vector2.UP)
 	
-	velocity = move_and_slide(velocity, Vector2.UP)
+	pass
