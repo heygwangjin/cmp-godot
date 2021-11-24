939218d09aa43e51b6becd81d4dbe411ed01b9d4
heygwangjin <cer3@ajou.ac.kr>
2021-11-24 17:12:41 +0900

    fix: Fix soul scoring (acquire score when collide with player only)

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -5,6 +5,6 @@ var souls = 0
 func _ready():
 	$souls.text = String(souls)
 
-func _on_soul_soul_collected2():
+func _on_soul_soul_collected():
 	souls = souls + 100
 	_ready()
--- Scripts/soul.gd
+++ Scripts/soul.gd
@@ -3,5 +3,6 @@ extends Area2D
 signal soul_collected
 
 func _on_soul_body_entered(body):
-	queue_free()
-	emit_signal("soul_collected")
+	if body.name == "Player":
+		queue_free()
+		emit_signal("soul_collected")
