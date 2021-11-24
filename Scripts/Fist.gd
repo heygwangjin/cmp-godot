0e3045b21f2fa2d3726b11c26ec4330eccce99e0
heygwangjin <cer3@ajou.ac.kr>
2021-11-24 15:01:34 +0900

    feat: Add health of player and falling fist

---
+++ Scripts/Fist.gd
@@ -0,0 +1,15 @@
+# Created by Gwang Jin Kim
+extends Area2D
+
+var velocity = Vector2()
+
+func _on_Fist_body_entered(body):
+	if body.name == "Player":
+		body.health -= 1
+		queue_free()
+	else:
+		queue_free()
+
+func _physics_process(delta):
+	 velocity.y += gravity
+	 position.y += velocity.y * delta
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,26 +1,41 @@
+# Created by Gwang Jin Kim
+
 extends KinematicBody2D
 
-const ACCELARATION = 300
-const GRAVITY = 20
-const JUMP_SPEED = 600
-var motion = Vector2()
+class_name Player
 
-func _physics_process(delta):
+const WALK_SPEED = 300
+const JUMP_SPEED = 600
+const GRAVITY = 20
+var velocity = Vector2()
+var health = 3
+
+func is_dead() -> bool:
+	if (health == 0):
+		return true
+	else:
+		return false
+
+func get_input():
 	# Movements
-	motion.y += GRAVITY
+	velocity.y += GRAVITY
 	if Input.is_action_pressed("ui_right"):
-		motion.x = ACCELARATION
+		velocity.x = WALK_SPEED
 		$Sprite.play("walk")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -ACCELARATION
+		velocity.x = -WALK_SPEED
 		$Sprite.play("walk")
 	else:
 		$Sprite.play("idle")
 		
 	if Input.is_action_pressed("jump") and is_on_floor():
-		motion.y -= JUMP_SPEED
-		
-	motion.x = lerp(motion.x,0,0.4)
-	motion = move_and_slide(motion, Vector2.UP)
+		velocity.y -= JUMP_SPEED
+
+func _physics_process(delta):
+	get_input()
+	if(is_dead()):
+		# Retry 화면 띄우기
+		queue_free()
 	
-	pass
+	velocity.x = lerp(velocity.x,0,0.4) # smooth stop
+	velocity = move_and_slide(velocity, Vector2.UP)
