a9dce90f6661601d95011161257be408842c1920
heygwangjin <cer3@ajou.ac.kr>
2021-11-30 15:40:39 +0900

    feat: Add minimum score that player needs to move on to the next stage

--- Scripts/King.gd
+++ Scripts/King.gd
@@ -1,4 +1,12 @@
 extends Area2D
 
+const min_souls = 400
+
 func _on_Jingwang_body_entered(body):
-   get_tree().change_scene("res://Hells/HellOfInjustice.tscn")
+	var souls = $"../HUD".souls
+	if (souls >= min_souls):
+		# 클리어 했을 때 나는 소리 구현
+		get_tree().change_scene("res://Hells/HellOfInjustice.tscn")
+	else:
+		# 죽을 때 나는 소리 구현
+		get_tree().reload_current_scene()
