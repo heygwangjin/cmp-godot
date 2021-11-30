61133bbcfbd31f67e6b6c14af11da391202ed8e0
heygwangjin <cer3@ajou.ac.kr>
2021-11-30 16:00:28 +0900

    fix: Fix error

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -2,12 +2,10 @@ extends CanvasLayer
 
 var souls = 0
 onready var health_player = $"../Player".health
-onready var min_souls = $"../Jingwang".min_souls
 
 func _ready():
 	$souls.text = String(souls)
 	$Live.text = "Lives : " + String(health_player)
-	$Goal.text = "Goal Souls : " + String(min_souls)
 
 func _on_soul_soul_collected():
 	souls = souls + 100
