bac310cd6127711fbe77f804165ebe791183be0e
yumi-jun <junyumi1006@naver.com>
2021-12-01 19:04:06 +0900

    메인씬

--- Intro/Intro_des.gd
+++ Intro/Intro_des.gd
@@ -6,4 +6,4 @@ func _ready():
 	yield(get_tree().create_timer(2),"timeout")
 	$AnimationPlayer.play("Fadeout2")
 	yield(get_tree().create_timer(2),"timeout")
-	get_tree().change_scene("res://Hells/HellOfViolence.tscn")
+	
--- Intro/Intro_start.gd
+++ Intro/Intro_start.gd
@@ -3,7 +3,15 @@ extends Node2D
 
 func _ready():
 	$AnimationPlayer.play("FadeIn")
-	yield(get_tree().create_timer(4),"timeout")
+	yield(get_tree().create_timer(3),"timeout")
 	$AnimationPlayer.play("FadeOut")
 	yield(get_tree().create_timer(3),"timeout")
-	get_tree().change_scene("res://Intro/IntrO_des.tscn")
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Hells/HellOfViolence.tscn")
+
+
+
+func _on_Button2_pressed():
+	get_tree().change_scene("res://Intro/Intro_des.tscn")
