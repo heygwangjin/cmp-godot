b62351bab68b3432617fb9a50b73434f2365531a
heygwangjin <cer3@ajou.ac.kr>
2021-12-01 23:03:23 +0900

    fix: Fix some modification in the intro scene

--- Intro/Intro_des.gd
+++ Intro/Intro_des.gd
@@ -3,8 +3,8 @@ extends Node2D
 
 func _ready():
 	$AnimationPlayer.play("FadeIn")
-	yield(get_tree().create_timer(3),"timeout")
+	yield(get_tree().create_timer(7),"timeout")
 	$AnimationPlayer.play("Fadeout2")
-	yield(get_tree().create_timer(4),"timeout")
+	yield(get_tree().create_timer(3),"timeout")
 	get_tree().change_scene("res://Intro/Intro_start.tscn")
 	print("hi")
--- Intro/Intro_start.gd
+++ Intro/Intro_start.gd
@@ -1,17 +1,16 @@
 extends Node2D
 
+var start = false
 
 func _ready():
-	$AnimationPlayer.play("FadeIn")
-	yield(get_tree().create_timer(3),"timeout")
-	$AnimationPlayer.play("FadeOut")
-	yield(get_tree().create_timer(3),"timeout")
-
+	while(!start):
+		$AnimationPlayer.play("FadeIn")
+		yield(get_tree().create_timer(3),"timeout")
+		$AnimationPlayer.play("FadeOut")
+		yield(get_tree().create_timer(3),"timeout")
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Hells/HellOfViolence.tscn")
-
-
 
 func _on_Button2_pressed():
 	get_tree().change_scene("res://Intro/Intro_des.tscn")
