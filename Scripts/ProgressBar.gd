6ff6c0763f2a59a1b8369d7f1ef7ed41c755effc
heygwangjin <cer3@ajou.ac.kr>
2021-12-01 16:59:06 +0900

    feat: Add health bar of boss

---
+++ Scripts/ProgressBar.gd
@@ -0,0 +1,22 @@
+# Created by Gwang Jin Kim
+extends ProgressBar
+
+func _ready():
+	set_process(false)
+
+func set_bar_value(value_to_set):
+	value = value_to_set
+	$Timer.start()
+
+func _on_Timer_timeout():
+	set_process(true)
+
+func _process(delta):
+	$ProgressBar2.value = lerp($ProgressBar2.value, value, 0.1)
+	if($ProgressBar2.value - value <= 0.5):
+		$ProgressBar2.value = value
+		set_process(false)
+
+
+func _on_Button_pressed():
+	set_bar_value(value - 10)
